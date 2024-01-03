da97ad386aba1754149f712ba412e6a1bbf320ab
Luciana <l.lopesdealmeida25@gmail.com>
2024-01-03 01:52:03 +0000

    enemy health bar fix

--- Assets/Scripts/EnemyHealthbar.cs
+++ Assets/Scripts/EnemyHealthbar.cs
@@ -7,15 +7,21 @@ using UnityEngine.UI;
 public class EnemyHealthbar : MonoBehaviour
 {
     public Image healthbarForeground;
+    private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
         healthbarForeground.fillAmount = currentHealth / maxHealth;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - cam.transform.position);
     }
 }
