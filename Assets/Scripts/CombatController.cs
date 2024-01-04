4f58bd114cfbe066aa549c23aa949c674a130960
Luciana <l.lopesdealmeida25@gmail.com>
2024-01-04 00:56:05 +0000

    weapon switcher

--- Assets/Scripts/CombatController.cs
+++ Assets/Scripts/CombatController.cs
@@ -5,55 +5,99 @@ using UnityEngine;
 
 public class CombatController : MonoBehaviour
 {
-    public GameObject ShortSwordPrefab;
+    public GameObject[] Weapons;
     public bool canAttack = true;
     public float attackCooldown = 1.0f;
 
+    private GameObject activeWeapon;
     public bool isAttacking = false;
     public bool canSpawn = true; //checks if new enemy drop can be spawned
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Find the first active child and set it as the Weapon
+        SetActiveWeapon(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
-            if(canAttack){
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canAttack)
+            {
                 NormalAttack();
+            }
+        }
+
+        // Check if key 1 is pressed and activate the first child
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetActiveWeapon(0);
+        }
+        // Check if key 2 is pressed and activate the second child
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetActiveWeapon(1);
+        }
+        // Check if key 3 is pressed and activate the third child
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetActiveWeapon(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetActiveWeapon(3);
+        }
+    }
+
+    void SetActiveWeapon(int activeIndex)
+    {
+        for (int i = 0; i < Weapons.Length; i++)
+        {
+            if (i == activeIndex)
+            {
+                Weapons[i].SetActive(true);
+                activeWeapon = Weapons[i];  // Set the active weapon
+            }
+            else
+            {
+                Weapons[i].SetActive(false);
             }
         }
     }
 
-    public void NormalAttack(){
+    public void NormalAttack()
+    {
         isAttacking = true;
         // cannot attack while attacking
         canAttack = false;
 
         canSpawn = false;
         // adding animations 
-        Animator anim = ShortSwordPrefab.GetComponent<Animator>();
+        Animator anim = activeWeapon.GetComponent<Animator>();
         anim.SetTrigger("Attack");
         StartCoroutine(ResetAttackCooldown());
-        
+
     }
 
-    IEnumerator ResetAttackCooldown(){
+    IEnumerator ResetAttackCooldown()
+    {
         StartCoroutine(ResetAttackBool());
         StartCoroutine(ResetEnemyDropCD());
         yield return new WaitForSeconds(attackCooldown);
         canAttack = true;
     }
 
-    IEnumerator ResetEnemyDropCD(){
+    IEnumerator ResetEnemyDropCD()
+    {
         yield return new WaitForSeconds(attackCooldown * 1.5f);
-        canSpawn=true;
+        canSpawn = true;
     }
 
-    IEnumerator ResetAttackBool(){
+    IEnumerator ResetAttackBool()
+    {
         yield return new WaitForSeconds(attackCooldown);
         isAttacking = false;
     }
