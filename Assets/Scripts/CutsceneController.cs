1e4a74eb78c23de957e1c62e80eec3dd6c4a83aa
nafisa-x <n.a.shaikh@se20.qmul.ac.uk>
2024-01-06 14:41:26 +0000

    added Intro cutscene

---
+++ Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneController : MonoBehaviour
+{
+
+    public GameObject neighbour;
+    //cutscenes
+    public GameObject scene1;
+    public GameObject scene2;
+
+    //dialogues
+    public GameObject dialogue1;
+    public GameObject dialogue2;
+    public GameObject dialogue3;
+
+    //change to second scene
+    public void SecondScene()
+    {
+        scene2.SetActive(true);
+        dialogue2.SetActive(true);
+        GameObject.Find("Selection1").SetActive(false);
+    }
+
+    //change to third scene and add NPC character
+    public void ThirdScene()
+    {
+        neighbour.SetActive(true);
+        dialogue3.SetActive(true);
+        GameObject.Find("Selection2").SetActive(false);
+    }
+
+    //start level
+    public void StartLevel()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+}
